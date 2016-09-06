1c59d5107d6263ea613a4efb1e5b71746ee43ae0
mrteera <inbox@mrteera.com>
2016-09-07 03:22:31 +0700

    Fix ENV for test_helper

--- {{cookiecutter.project_name}}/test/test_helper.rb
+++ {{cookiecutter.project_name}}/test/test_helper.rb
@@ -1,3 +1,6 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
 require 'simplecov'
 require 'coveralls'
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
@@ -8,10 +11,6 @@ SimpleCov.start do
   add_filter 'app/jobs/application_job.rb'
   add_filter 'app/models/application_record.rb'
 end
-
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
